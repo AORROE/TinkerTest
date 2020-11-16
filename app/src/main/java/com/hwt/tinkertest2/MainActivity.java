69924a50fc005bf04ff2ad1112cdcc72458305c6
lsc1993 <liushuanggo@gmail.com>
2020-11-16 17:30:03 +0800

    基准包代码

--- app/src/main/java/com/hwt/tinkertest2/MainActivity.java
+++ app/src/main/java/com/hwt/tinkertest2/MainActivity.java
@@ -28,5 +28,6 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+        Toast.makeText(this,TestDelClass.getMyClassName(),Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/hwt/tinkertest2/TestDelClass.java
@@ -0,0 +1,7 @@
+package com.hwt.tinkertest2;
+
+public class TestDelClass {
+    public static String getMyClassName(){
+        return TestDelClass.class.getName();
+    }
+}
